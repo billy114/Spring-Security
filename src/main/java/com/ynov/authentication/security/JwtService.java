2283232a45eb0888449676f59a96b591c344ba17
Billel <billel.abbes@hotmail.com>
2024-03-14 17:01:36 +0100

    Secret key

--- src/main/java/com/ynov/authentication/security/JwtService.java
+++ src/main/java/com/ynov/authentication/security/JwtService.java
@@ -2,10 +2,12 @@ package com.ynov.authentication.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -15,29 +17,28 @@ import java.util.Map;
 
 @Service
 public class JwtService {
+    final static private SignatureAlgorithm alg = SignatureAlgorithm.HS256;
+    final static private SecretKey SECRET_KEY = generateSecretKey();
 
     public String generateToken(
             UserDetails userDetails,
             Map<String, String> extraClaims
-    ) throws NoSuchAlgorithmException {
+    ) {
         return Jwts
                 .builder()
                 .setSubject(userDetails.getUsername())
                 .setClaims(extraClaims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
-                .signWith(generateSecretKey() ,SignatureAlgorithm.HS256)
+                .signWith(SECRET_KEY ,alg)
                 .compact();
     }
 
-    public String generateToken(UserDetails userDetails) throws NoSuchAlgorithmException {
+    public String generateToken(UserDetails userDetails) {
         return generateToken(userDetails, new HashMap<>());
     }
 
-    private static Key generateSecretKey() throws NoSuchAlgorithmException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
-        SecureRandom secureRandom = new SecureRandom();
-        keyGenerator.init(secureRandom);
-        return keyGenerator.generateKey();
+    private static SecretKey generateSecretKey() {
+        return Keys.secretKeyFor(alg);
     }
 }
